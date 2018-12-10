9eb2efc92d362afba3cf10a6870bc5bf71987cf3
abdullah.ilhanli <abdullah.ilhanli@amadeus.com>
2018-12-10 16:50:14 +0300

    grouping

---
+++ src/main/java/com/ailhanli/practice4/Employee.java
@@ -0,0 +1,26 @@
+package com.ailhanli.practice4;
+
+public class Employee {
+	private String name;
+	private int age;
+	private double salary;
+
+	public Employee(String name, int age, double salary) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+}
--- src/main/java/com/ailhanli/practice4/PracticeGrouping.java
+++ src/main/java/com/ailhanli/practice4/PracticeGrouping.java
@@ -1,5 +1,30 @@
 package com.ailhanli.practice4;
 
-public class PracticeGrouping {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
+public class PracticeGrouping {
+	
+	public static void main(String[] args) {
+	
+		List<Employee> employees = Arrays.asList(
+				new Employee("employee1", 30, 2000),
+				new Employee("employee2", 23, 3000),
+				new Employee("employee3", 30, 2000),
+				new Employee("employee4", 23, 3000),
+				new Employee("employee5", 30, 4000),
+				new Employee("employee6", 40, 2000)
+				);
+				
+		employees.stream().collect(Collectors.groupingBy(Employee::getAge, Collectors.summingDouble(Employee::getSalary)));
+	}
+	
+//	public Map<Integer, Double> getSumofEmployeeSalaryByTheirAge(){
+//		
+//	
+//		
+//	}
 }
