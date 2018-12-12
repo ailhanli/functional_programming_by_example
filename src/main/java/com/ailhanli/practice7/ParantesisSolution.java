129874dc8c3da8ece13d04b95fd4831b6cc834e3
Abdullah Abi <abdullah.ilhanli@icloud.com>
2018-12-12 21:03:04 +0300

    cant functional

---
+++ src/main/java/com/ailhanli/practice7/ParantesisSolution.java
@@ -0,0 +1,27 @@
+package com.ailhanli.practice7;
+
+import java.util.Stack;
+
+public class ParantesisSolution {
+
+	public boolean isBalanced(String word) {
+		Stack<String> stack = new Stack<>();
+
+		for (int i = 0; i < word.length(); i++) {
+			String newSymbol = "" + word.charAt(i);
+
+			if (newSymbol.equals("(") || newSymbol.equals("[")) {
+				stack.push("" + newSymbol);
+			} else {
+				String firstSymbol = stack.pop();
+
+				if ((!firstSymbol.equals("(") && newSymbol.equalsIgnoreCase(")"))
+						|| (!firstSymbol.equals("[") && newSymbol.equalsIgnoreCase("]"))) {
+					return false;
+				}
+			}
+		}
+
+		return stack.empty();
+	}
+}
